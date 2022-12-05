4cef2eb969c1f4eec266406da4aad70d73404809
NetLops <87888892+NetLops@users.noreply.github.com>
2022-12-05 10:48:10 +0800

    Update download.sh

--- tools/seafile/download.sh
+++ tools/seafile/download.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -e
-URL=`curl -s -i $1|grep Location|awk '{print $2}'|tr -d '\r'`
+URL=`curl -s -i $1|grep -i location|awk '{print $2}'|tr -d '\r'`
 curl -O $URL
 echo -e "save as\033[31m ${URL##*/}  \033[0m" 
